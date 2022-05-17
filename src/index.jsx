ce04d1a40c0442bcce3c0ea5f3369bf76f2a9b29
bvbsis <mr.topal89@gmail.com>
2022-05-17 14:06:07 +0300

    package fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { compose, createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import "./index.css";
 import App from "./components/app/app";
-
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-// const store = createStore(rootReducer, enhancer);
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Provider } from "react-redux";
+import store from "./services/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
